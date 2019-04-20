ca65ed0145e7432bc8d53e69b66abb7e695cefb1
Nigum Dave <ndd25@njit.edu>
2019-04-20 19:43:20 -0400

    Generate the authentication

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class HomeController extends Controller
 {
@@ -24,5 +25,12 @@ class HomeController extends Controller
     public function index()
     {
         return view('home');
+    }
+
+    public function profile()
+    {
+        $user = $user = Auth::user();
+        dd($user);
+        return view('profile', ['myvariable' => 'Hello World!'] );
     }
 }
